f94566bd1698679de33ff8023cb17bb5265a09ec
Gabriel Pages <gbpages@yahoo.com>
2016-03-03 20:11:10 -0500

    worked on project

---
+++ app/scripts/index.js
@@ -0,0 +1,53 @@
+var $ = require('jquery');
+var _ = require('underscore');
+
+
+$('#start').on('click',function(){
+  $('#start').trigger('hero attacked');
+  bindIt();
+});
+
+function bindIt(){
+  $('#start').bind('hero attacked', heroAttack());
+}
+
+function unbindIt(){
+  $('#start').unbind('hero attacked');
+}
+
+function heroAttack(){
+  unbindIt();
+  var randomNum = _.random(0,10);
+  $('.content-section-enemy').append('<p>"BAHHHHH! You attacked me!!!"</p>' + '( -' + randomNum + '%)');
+  var progBarStat = $('#enemy-prog-bar').html();
+  var ProgBarInt = parseFloat(progBarStat);
+  var newProgBarStat = ProgBarInt - randomNum;
+  var enemysBar =$('#enemy-prog-bar').html(newProgBarStat + "%");
+  $('#enemy-prog-bar').css({"width": newProgBarStat + '%'})
+  if (parseFloat(enemysBar[0].textContent) <= 0){
+    alert("Congradulations!!! You've bet Starring Suzie!");
+  }else{
+    setTimeout(enemyAttack, 2000);
+  }
+}
+
+function enemyAttack(){
+  var randomNum = _.random(0, 10);
+  $('.content-section-hero').append('<p>"AHHHHH! The Enemy attacked you!!!"</p>' + '( -' + randomNum + '%)');
+  var progBarStat = $('#hero-prog-bar').html();
+  var ProgBarInt = parseFloat(progBarStat);
+  var newProgBarStat = ProgBarInt - randomNum;
+  var herosBar = $('#hero-prog-bar').html(newProgBarStat + "%");
+  $('#hero-prog-bar').css({"width": newProgBarStat + '%'})
+  if (parseFloat(herosBar[0].textContent) <= 0){
+    alert("Rats!!! You've been beaten by Starring Suzie!");
+  }else{
+    setTimeout(clearFields, 2000);
+  }
+
+}
+
+function clearFields(){
+  $('.content-section-enemy').html('');
+  $('.content-section-hero').html('');
+}
